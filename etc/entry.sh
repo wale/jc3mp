4994f5ac660e5a857597d0f7b2787912b865330c
Duale Siad <me@wale.id.au>
2021-09-29 17:57:44 +1000

    attempt to make a fuckin' jc3mp image for docker

--- etc/entry.sh
+++ etc/entry.sh
@@ -11,28 +11,25 @@ else
 fi
 
 # We assume that if the config is missing, that this is a fresh container
-if [ ! -f "${STEAMAPPDIR}/cfg/Game.ini" ]; then
-	# Download & extract the configs
-	wget -qO- "${DLURL}/master/etc/cfg.tar.gz" | tar xvzf - -C "${STEAMAPPDIR}/"
-	
-	# Change first launch variables (you can comment this out if it has done it's purpose)
-	sed -i -e 's/{{SERVER_PW}}/'"${SERVER_PW}"'/g' \
-			-e 's/{{SERVER_ADMINPW}}/'"${SERVER_ADMINPW}"'/g' \
-			-e 's/{{SERVER_NAME}}/'"${SERVER_NAME}"'/g' \
-			-e 's/{{SERVER_MAXPLAYERS}}/'"${SERVER_MAXPLAYERS}"'/g' "${STEAMAPPDIR}/cfg/Game.ini"
-
-	sed -i -e 's/{{SERVER_TICKRATE}}/'"${SERVER_TICKRATE}"'/g' \
-			-e 's/{{SERVER_DEFAULTMAP}}/'"${SERVER_DEFAULTMAP}"'/g' "${STEAMAPPDIR}/cfg/Engine.ini"
+if [ ! -f "${STEAMAPPDIR}/config.json" ]; then
+	jq --null-input \
+		--arg announce false \
+		--arg description "${SERVER_DESCRIPTION}" \
+		--arg host "0.0.0.0" \
+		--arg httpPort $SERVER_HTTPORT
+		--arg logLevel 7
+		--arg logo ""
+		--arg maxPlayers $SERVER_MAXPLAYERS
+		--arg maxTickRate $SERVER_TICKRATE
+		--arg name $SERVER_NAME
+		--arg name $SERVER_PASS
+		--arg port $SERVER_PORT
+		--arg queryPort $SERVER_QUERYPORT
+		--arg requiredDLC []
+		--arg steamPort $SERVER_STEAMPORT
 fi
 
 # Switch to workdir
 cd "${STEAMAPPDIR}"
 
-bash "${STEAMAPPDIR}/MordhauServer.sh" \
-			-log \
-			-Port="${SERVER_PORT}" \
-			-QueryPort="${SERVER_QUERYPORT}" \
-			-BeaconPort="${SERVER_BEACONPORT}" \
-			-GAMEINI="${STEAMAPPDIR}/${SERVER_GAMEINI}" \
-			-ENGINEINI="${STEAMAPPDIR}/${SERVER_ENGINEINI}" \
-			"${ADDITIONAL_ARGS}"
+bash "${STEAMAPPDIR}/Server"
