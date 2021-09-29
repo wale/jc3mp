e72406b54826f79286ecc3da032fe76b87256300
Duale Siad <me@wale.id.au>
2021-09-29 19:43:44 +1000

    more bullshit

--- etc/entry.sh
+++ etc/entry.sh
@@ -10,15 +10,11 @@ else
 	bash "${STEAMCMDDIR}/steamcmd.sh" +login anonymous +force_install_dir "$STEAMAPPDIR" +app_update "$STEAMAPPID" "${steamcmd_update_args[@]}" +quit
 fi
 
-# We assume that if the config is missing, that this is a fresh container
-if [ ! -f "${STEAMAPPDIR}/config.json" ]; then
-	template='{"announce":false,"description":"%s","host":"0.0.0.0","httpPort":%s,"logLevel":7,"logo":"","maxPlayers":%s,"maxTickRate":%s,"name":"%s","pass":%s,"port":%s,"queryPort":%s, "requiredDLC": [], "steamPort": %s}'
-	json_string=$(printf "$template" $SERVER_DESCRIPTION "$SERVER_HTTPPORT" "$SERVER_MAXPLAYERS" "$SERVER_MAXTICKRATE" "$SERVER_NAME" "$SERVER_PASS" "$SERVER_PORT" "$SERVER_QUERYPORT" "$SERVER_STEAMPORT")
-	echo ${json_string} > ${STEAMAPPDIR}/config.json
-fi
+# do some stupid shit to the json file
+sed -i "s/{description}/${SERVER_DESCRIPTION}/" ${STEAMAPPDIR}/config.json
+sed -i "s/{name}/${SERVER_NAME}" ${STEAMAPPDIR}/config.json
+sed -i "s/{pass}/${SERVER_PASS}/" ${STEAMAPPDIR}/config.json
+
 
 # Switch to workdir
-cd "${STEAMAPPDIR}"
-
-# run server
-./Server
+cd "${STEAMAPPDIR}" && ./Server
